5a3242da66bcc2c90f91aa378cfccb5b74ad1ac0
AhmadOsama <ahmad.osama.moh@gmail.com>
2018-09-04 23:01:49 +0200

    Implement pawn move generation logic

--- js/board.js
+++ js/board.js
@@ -1,15 +1,24 @@
+var squaresIDs = new Array(8);
+var boardPieces = new Array(8);
 (function initBoard(){
+    
     var board = document.getElementById('chessBoard');
     var isWhite = true;
-    var squaresIDs = new Array(8);
-    //draw squares
-    for(var i = 0; i < 8; i++){
-        squaresIDs[i] = new Array(8);
-        var row = String.fromCharCode('A'.charCodeAt() + i);
 
-        for(var j = 0; j < 8; j++){
-            var col = String(j);
+    var attackedWhitePieces = [];
+    var attackedBlackPieces = [];
+
+    //draw squares
+    for(let i = 0; i < 8; i++){
+        squaresIDs[i] = new Array(8);
+        boardPieces[i] = new Array(8);
+
+        let row = String.fromCharCode('A'.charCodeAt() + i);
+
+        for(let j = 0; j < 8; j++){
+            let col = String(j);
             squaresIDs[i][j] = row + col;
+            boardPieces[i][j] = null;
 
             square = document.createElement('div');
             square.id = squaresIDs[i][j];
@@ -19,7 +28,7 @@
             else
                 square.classList.add('square', 'black');
             
-            console.log('row: ' + row + ' col: ' + col + ' ' + squaresIDs[i][j]);
+            // console.log('row: ' + row + ' col: ' + col + ' ' + squaresIDs[i][j]);
             board.appendChild(square);
             isWhite = !isWhite;
         }
--- js/pieces.js
+++ js/pieces.js
@@ -1,7 +1,7 @@
 // base for all chess pieces
 var ColorsEnum = Object.freeze({
     'BLACK': 0,
-    'WHITE': 0
+    'WHITE': 1
 })
 
 var PiecesEnum = Object.freeze({
@@ -22,20 +22,59 @@ var Piece = function(row, col, type, color){
     this.isAlive = true;
 }
 
+Piece.prototype.removePiece = function(){
+    this.isAlive = false;
+    if(this.color == ColorsEnum.WHITE){
+        attackedWhitePieces.push(this);
+    }
+    else{
+        attackedBlackPieces.push(this);
+    }
+    boardPieces[this.row][this.col] = null;
+}
+
+Piece.prototype.movePiece = function(move){
+    boardPieces[this.row][this.col] = null;
+    this.row = move.to[0];
+    this.col = move.to[1];
+
+    if(move.attack !== null)
+        move.attack.removePiece();
+    
+    boardPieces[this.row][this.col] = this;
+}
+
+Piece.prototype.getPieceDivID = function(){
+    let row = String.fromCharCode('A'.charCodeAt() + this.row);
+    let col = String(this.col);
+
+    id = row + col;
+
+    return id;
+}
+
 // Move class
-var Move = function(from, to, piece)
+var Move = function(from, to, piece, attack)
 {
-    this.from = from;
-    this.to = to;
-    this.piece = piece;
+    this.from = from; // (x, y)
+    this.to = to; // (x, y)
+    this.piece = piece; // piece
+    this.attack = attack // null => no attack, pieceAttacked otherwise
 }
 
 // Implement each Piece moves
 
 // PAWN class
 var Pawn = function(row, col, color){
+    console.log(color);
     Piece.call(this, row, col, PiecesEnum.PAWN, color)
     this.doneMove = false;
+    
+    this.image = 'assets/Pawn-White.png';
+    if(this.color === ColorsEnum.BLACK)
+        this.image = 'assets/Pawn-Black.png';
+
+    boardPieces[row][col] = this;
 }
 
 Pawn.prototype = Object.create(Piece.prototype);
@@ -43,23 +82,85 @@ Pawn.prototype = Object.create(Piece.prototype);
 Pawn.prototype.getValidMoves = function(){
     let validMoves = [];
     
-    if(this.color === ColorsEnum.WHITE){
-        if(this.doneMove === false){ // can move 2 squares forward
-
-        }
-        else{
-
-        }
-    }
-    else{
-        if(this.doneMove === false){
-
-        }
-        else{
+    if(this.color === ColorsEnum.WHITE){ // White Pawn
+        // move 1 step forward
+        if(this.row > 0 && boardPieces[this.row - 1][this.col] === null){ 
+            let move = new Move([this.row, this.col],
+                                [this.row - 1, this.col],
+                                this,
+                                null
+                            );
             
+            validMoves.push(move);
         }
+
+        // move 2 steps forward
+        if(this.doneMove === false && boardPieces[this.row - 1][this.col] === null 
+            && boardPieces[this.row - 2][this.col] === null) // not yet moved
+        { 
+            let move = new Move([this.row, this.col],
+                [this.row - 2, this.col],
+                this,
+                null
+            );
+            validMoves.push(move);
+        }
+
+        // Attack other pieces
+        for(let j = -1; j < 2; j += 2){
+            if(this.col + j < 8 && this.col + j >= 0 && this.row > 0 && boardPieces[this.row - 1][this.col + j] !== null
+               && boardPieces[this.row - 1][this.col + j].color !== this.color)
+            {
+     
+                 let move = new Move([this.row, this.col],
+                     [this.row - 1, this.col + j],
+                     this,
+                     boardPieces[this.row - 1][this.col + j]
+                 );
+                 validMoves.push(move);
+             }
+        }        
+    }
+    else{ // Black Pawn
+        // move 1 step forward
+        if(this.row < 7 && boardPieces[this.row + 1][this.col] === null){ // move 1 step forward
+            let move = new Move([this.row, this.col],
+                                [this.row + 1, this.col],
+                                this,
+                                null
+                            );
+
+            validMoves.push(move);
+        }
+
+         // move 2 steps forward
+        if(this.doneMove === false && boardPieces[this.row + 1][this.col] === null 
+            && boardPieces[this.row + 2][this.col] === null) // not yet moved
+        { 
+            let move = new Move([this.row, this.col],
+                [this.row + 2, this.col],
+                this,
+                null
+            );
+
+            validMoves.push(move);
+        }
+
+        // Attack other pieces
+        for(let j = -1; j < 2; j += 2){
+            if(this.col + j < 8 && this.col + j >= 0 && this.row < 7 && boardPieces[this.row + 1][this.col + j] !== null
+               && boardPieces[this.row + 1][this.col + j].color !== this.color)
+            {
+     
+                 let move = new Move([this.row, this.col],
+                     [this.row + 1, this.col + j],
+                     this,
+                     boardPieces[this.row + 1][this.col + j]
+                 );
+                 validMoves.push(move);
+             }
+        }        
     }
 
     return validMoves;
 }
-
